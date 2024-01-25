61478009709469aa804b4954a01ec632823da37f
Tan-dat2001 <nguyentandat23122001@gmail.com>
2024-01-25 10:11:47 +0700

    feat: contact us component

--- src/utils/properties.ts
+++ src/utils/properties.ts
@@ -42,7 +42,57 @@ export const properties: Properties[] = [
     numberOfFloor: 1,
     createdAt: '2023-02-01T10:00:00Z',
     updatedAt: '2023-02-05T12:45:00Z',
-    description: 'A modern apartment in a convenient location.',
+    description: `
+    Resale Apartment Da Nang in Fusion Suites, Son Tra District, walk to My Khe Beach
+    This residence flat provides entirely comfortable feeling of its interiors and architects
+    Fully-furnished, 2-bedrooms, 2-bathroom, balcony, kitchen area, laundry, over 62sqm
+    RESALE APARTMENT DA NANG
+    Apartments located in the vicinity of essential destination, right at the white sandy beach, 10 minutes from the airport, tasteful seafood restaurants are in few steps from your door;
+    Decoration are harmonious, designing in an open way, sense of airy make you feel at home
+    Internal amenities are superb, besides from spectacular view towards the sea of the area, it offers free infinite swimming pool, fitness center as well as bring you closer to natural with plenty of trees
+    Fusion Suites and Villa is one of the most luxury, promising and interesting beachfront project that attracts investors, suitable for living, will not let you down,
+    Available for Sale: 3.2 billion VND
+    
+    Viet Nam ownership permanent, can handover to expats
+    
+    Schedule visiting or further details Feel free to give a call or direct messgage to me, let me know your request and we will guide you home.
+    
+    Find us on Facebook
+    
+    Contact number 0917 112 855 (WhatsApp-Zalo-Kaotalk-Viber-Wechat)
+    Resale Apartment Da Nang in Fusion Suites, Son Tra District, walk to My Khe Beach
+    This residence flat provides entirely comfortable feeling of its interiors and architects
+    Fully-furnished, 2-bedrooms, 2-bathroom, balcony, kitchen area, laundry, over 62sqm
+    RESALE APARTMENT DA NANG
+    Apartments located in the vicinity of essential destination, right at the white sandy beach, 10 minutes from the airport, tasteful seafood restaurants are in few steps from your door;
+    Decoration are harmonious, designing in an open way, sense of airy make you feel at home
+    Internal amenities are superb, besides from spectacular view towards the sea of the area, it offers free infinite swimming pool, fitness center as well as bring you closer to natural with plenty of trees
+    Fusion Suites and Villa is one of the most luxury, promising and interesting beachfront project that attracts investors, suitable for living, will not let you down,
+    Available for Sale: 3.2 billion VND
+    
+    Viet Nam ownership permanent, can handover to expats
+    
+    Schedule visiting or further details Feel free to give a call or direct messgage to me, let me know your request and we will guide you home.
+    
+    Find us on Facebook
+    
+    Contact number 0917 112 855 (WhatsApp-Zalo-Kaotalk-Viber-Wechat)Resale Apartment Da Nang in Fusion Suites, Son Tra District, walk to My Khe Beach
+    This residence flat provides entirely comfortable feeling of its interiors and architects
+    Fully-furnished, 2-bedrooms, 2-bathroom, balcony, kitchen area, laundry, over 62sqm
+    RESALE APARTMENT DA NANG
+    Apartments located in the vicinity of essential destination, right at the white sandy beach, 10 minutes from the airport, tasteful seafood restaurants are in few steps from your door;
+    Decoration are harmonious, designing in an open way, sense of airy make you feel at home
+    Internal amenities are superb, besides from spectacular view towards the sea of the area, it offers free infinite swimming pool, fitness center as well as bring you closer to natural with plenty of trees
+    Fusion Suites and Villa is one of the most luxury, promising and interesting beachfront project that attracts investors, suitable for living, will not let you down,
+    Available for Sale: 3.2 billion VND
+    
+    Viet Nam ownership permanent, can handover to expats
+    
+    Schedule visiting or further details Feel free to give a call or direct messgage to me, let me know your request and we will guide you home.
+    
+    Find us on Facebook
+    
+    Contact number 0917 112 855 (WhatsApp-Zalo-Kaotalk-Viber-Wechat)`,
     images:
       'https://toanhuyhoang.com/wp-content/uploads/2023/03/ban-can-ho-luxury-da-nang.jpg',
   },
@@ -68,7 +118,7 @@ export const properties: Properties[] = [
     images:
       'https://toanhuyhoang.com/wp-content/uploads/2023/03/ban-can-ho-tai-alphanam-luxury-apartment.jpg',
   },
-  
+
   {
     propertyId: 4,
     name: 'Apartment for sale at Alphanam Luxury Apartment Danang ',
