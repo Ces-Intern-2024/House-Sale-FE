d4f42d90bf1604d4524eeecbd0e86821737d1f9e
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2024-02-20 10:15:22 +0700

    feat: search-page

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,42 @@
 import React, { useState } from 'react'
-import { Menu, Button, Transition } from '@mantine/core'
-import { IconChevronDown } from '@tabler/icons-react'
+import {
+  Transition,
+  Select,
+  TextInput,
+  ComboboxItem,
+  OptionsFilter,
+  Button,
+} from '@mantine/core'
+// import { IconChevronDown } from '@tabler/icons-react'
 import styles from './SearchBar.module.scss'
-import { FaAlignJustify, FaSearch } from 'react-icons/fa'
+import { FaAlignJustify, FaSearch, FaBath, FaBed, FaHome } from 'react-icons/fa'
+import {
+  useFetchProvincesQuery,
+  useFetchDistrictsQuery,
+  useFetchWardsQuery,
+} from '../../redux/reducers/locationSlice'
+import { useForm } from '@mantine/form'
+
+const optionsFilter: OptionsFilter = ({ options, search }) => {
+  const splittedSearch = search.toLowerCase().trim().split(' ')
+  return (options as ComboboxItem[]).filter((option) => {
+    const words = option.label.toLowerCase().trim().split(' ')
+    return splittedSearch.every((searchWord) =>
+      words.some((word) => word.includes(searchWord)),
+    )
+  })
+}
 
 export default function SearchBar() {
   const [isExpanded, setIsExpanded] = useState(false)
+  const { data: provinces = [] } = useFetchProvincesQuery()
+  const [provinceCode, setProvinceCode] = useState('')
+  const { data: districts = [] } = useFetchDistrictsQuery(provinceCode)
+  const [districtCode, setDistrictCode] = useState<string | null>('')
+  const { data: wards = [] } = useFetchWardsQuery(districtCode)
+  const [wardCode, setWardCode] = useState<string | null>('')
+
+  const form = useForm({})
 
   const handleExpandSearch = () => {
     setIsExpanded((prev) => !prev)
@@ -14,6 +45,108 @@ export default function SearchBar() {
   return (
     <>
       <div className={styles.outer}>
+        <div className="grid grid-cols-2 md:grid-cols-5 ">
+          <TextInput
+            leftSection={<FaSearch></FaSearch>}
+            classNames={{ input: styles.inputOuter }}
+            placeholder="727 Main Street"
+            defaultValue=""
+            {...form.getInputProps('')}
+          />
+
+          <Select
+            classNames={{ input: styles.btnOuter }}
+            checkIconPosition="right"
+            placeholder="City/Province"
+            data={provinces.flatMap((prov: any) => [
+              {
+                value: prov.provinceCode,
+                label: prov.nameEn,
+              },
+            ])}
+            filter={optionsFilter}
+            searchable
+            comboboxProps={{
+              position: 'bottom',
+              offset: 0,
+              transitionProps: { transition: 'pop', duration: 200 },
+            }}
+            {...form.getInputProps('provinceCode')}
+            onChange={(_value: any) => {
+              form.setFieldValue('provinceCode', _value)
+              setProvinceCode(_value)
+              setDistrictCode(null)
+              setWardCode(null)
+            }}
+            value={provinceCode}
+          />
+
+          <Select
+            classNames={{ input: styles.btnOuter }}
+            checkIconPosition="right"
+            placeholder="District"
+            data={districts.flatMap((dist: any) => [
+              {
+                value: dist.districtCode,
+                label: dist.nameEn,
+              },
+            ])}
+            filter={optionsFilter}
+            searchable
+            comboboxProps={{
+              position: 'bottom',
+              offset: 0,
+              transitionProps: { transition: 'pop', duration: 200 },
+            }}
+            {...form.getInputProps('districtCode')}
+            onChange={(_value: any) => {
+              form.setFieldValue('districtCode', _value)
+              setDistrictCode(_value)
+              setWardCode(null)
+            }}
+            value={districtCode}
+          />
+
+          <Select
+            classNames={{ input: styles.btnOuter }}
+            checkIconPosition="right"
+            placeholder="Ward"
+            data={wards.flatMap((ward: any) => [
+              {
+                value: ward.wardCode,
+                label: ward.nameEn,
+              },
+            ])}
+            filter={optionsFilter}
+            searchable
+            comboboxProps={{
+              position: 'bottom',
+              offset: 0,
+              transitionProps: { transition: 'pop', duration: 200 },
+            }}
+            {...form.getInputProps('wardCode')}
+            onChange={(_value: any) => {
+              form.setFieldValue('wardCode', _value)
+              setWardCode(_value)
+            }}
+            value={wardCode}
+          />
+
+          <div className={styles.flexBox}>
+            <Button
+              className={styles.searchButton2}
+              onClick={handleExpandSearch}
+            >
+              <FaAlignJustify />
+            </Button>
+            <Button
+              rightSection={<FaSearch></FaSearch>}
+              className={styles.searchButton2}
+            >
+              Search
+            </Button>
+          </div>
+        </div>
         <div className={isExpanded ? 'visible' : 'invisible'}>
           <Transition
             mounted={isExpanded ? true : false}
@@ -23,160 +156,72 @@ export default function SearchBar() {
           >
             {(stylesParam) => (
               <div style={stylesParam}>
-                <div className="grid grid-cols-2 md:grid-cols-4">
-                  <Menu
-                    trigger="click"
-                    openDelay={100}
-                    closeDelay={400}
-                    width={200}
-                    withArrow
-                  >
-                    <Menu.Target>
-                      <div className={styles.btnOuter}>
-                        <Button className={styles.searchBtn}>BED</Button>
-                        <IconChevronDown />
-                      </div>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                      <Menu.Item>Search</Menu.Item>
-                      <Menu.Item>House</Menu.Item>
-                      <Menu.Item>Apartment</Menu.Item>
-                    </Menu.Dropdown>
-                  </Menu>
+                <div className="grid grid-cols-2 md:grid-cols-5">
+                  <Select
+                    classNames={{ input: styles.btnOuter }}
+                    rightSection={<h1>m²</h1>}
+                    allowDeselect={false}
+                    checkIconPosition="right"
+                    placeholder="Area 50m²"
+                    data={['30', '50', '70', '90', '120', '150', '200']}
+                  />
+                  <Select
+                    classNames={{ input: styles.btnOuter }}
+                    leftSection={<FaBed></FaBed>}
+                    allowDeselect={false}
+                    checkIconPosition="right"
+                    placeholder="Bed"
+                    data={['1', '2', '3', '4', '5', '6', '7']}
+                  />
 
-                  <Menu
-                    trigger="click"
-                    openDelay={100}
-                    closeDelay={400}
-                    width={200}
-                    withArrow
-                  >
-                    <Menu.Target>
-                      <div className={styles.btnOuter}>
-                        <Button className={styles.searchBtn}>BATHROOM</Button>
-                        <IconChevronDown />
-                      </div>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                      <Menu.Item>Search</Menu.Item>
-                      <Menu.Item>House</Menu.Item>
-                      <Menu.Item>Apartment</Menu.Item>
-                    </Menu.Dropdown>
-                  </Menu>
+                  <Select
+                    classNames={{ input: styles.btnOuter }}
+                    leftSection={<FaBath></FaBath>}
+                    allowDeselect={false}
+                    checkIconPosition="right"
+                    placeholder="Bathroom"
+                    data={['1', '2', '3', '4', '5', '6', '7']}
+                  />
 
-                  <Menu
-                    trigger="click"
-                    openDelay={100}
-                    closeDelay={400}
-                    width={200}
-                    withArrow
-                  >
-                    <Menu.Target>
-                      <div className={styles.btnOuter}>
-                        <Button className={styles.searchBtn}>CATEGORY</Button>
-                        <IconChevronDown />
-                      </div>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                      <Menu.Item>Search</Menu.Item>
-                      <Menu.Item>House</Menu.Item>
-                      <Menu.Item>Apartment</Menu.Item>
-                    </Menu.Dropdown>
-                  </Menu>
-
-                  <Menu
-                    trigger="click"
-                    openDelay={100}
-                    closeDelay={400}
-                    width={200}
-                    withArrow
-                  >
-                    <Menu.Target>
-                      <div className={styles.btnOuter}>
-                        <Button className={styles.searchBtn}>RENT/SALE</Button>
-                        <IconChevronDown />
-                      </div>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                      <Menu.Item>Search</Menu.Item>
-                      <Menu.Item>House</Menu.Item>
-                      <Menu.Item>Apartment</Menu.Item>
-                    </Menu.Dropdown>
-                  </Menu>
+                  <Select
+                    classNames={{ input: styles.btnOuter }}
+                    leftSection={<FaHome></FaHome>}
+                    checkIconPosition="right"
+                    placeholder="Category"
+                    // data={wards.flatMap((ward: any) => [
+                    //   {
+                    //     value: ward.wardCode,
+                    //     label: ward.nameEn,
+                    //   },
+                    // ])}
+                    filter={optionsFilter}
+                    searchable
+                    comboboxProps={{
+                      position: 'bottom',
+                      offset: 0,
+                      transitionProps: { transition: 'pop', duration: 200 },
+                    }}
+                    {...form.getInputProps('wardCode')}
+                    onChange={(_value: any) => {
+                      form.setFieldValue('wardCode', _value)
+                      setWardCode(_value)
+                    }}
+                    value={wardCode}
+                  />
+                  <Select
+                    classNames={{ input: styles.btnOuter }}
+                    checkIconPosition="right"
+                    placeholder="For Rent/Sale"
+                    data={['For Rent', 'For Sale']}
+                    onChange={(_value: any) => {
+                      form.setFieldValue('wardCode', _value)
+                      setWardCode(_value)
+                    }}
+                  />
                 </div>
               </div>
             )}
           </Transition>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 ">
-          <Menu
-            trigger="click"
-            openDelay={100}
-            closeDelay={400}
-            width={200}
-            withArrow
-          >
-            <Menu.Target>
-              <div className={styles.btnOuter}>
-                <Button className={styles.searchBtn}>PROVINCE</Button>
-                <IconChevronDown />
-              </div>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item>Search</Menu.Item>
-              <Menu.Item>House</Menu.Item>
-              <Menu.Item>Apartment</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-
-          <Menu
-            trigger="click"
-            openDelay={100}
-            closeDelay={400}
-            width={200}
-            withArrow
-          >
-            <Menu.Target>
-              <div className={styles.btnOuter}>
-                <Button className={styles.searchBtn}>DISTRICT</Button>
-                <IconChevronDown />
-              </div>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item>Search</Menu.Item>
-              <Menu.Item>House</Menu.Item>
-              <Menu.Item>Apartment</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-
-          <Menu
-            trigger="click"
-            openDelay={100}
-            closeDelay={400}
-            width={200}
-            withArrow
-          >
-            <Menu.Target>
-              <div className={styles.btnOuter}>
-                <Button className={styles.searchBtn}>WARD</Button>
-                <IconChevronDown />
-              </div>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item>Search</Menu.Item>
-              <Menu.Item>House</Menu.Item>
-              <Menu.Item>Apartment</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-
-          <div className={styles.flexBox}>
-            <button className={styles.btnOuter} onClick={handleExpandSearch}>
-              <FaAlignJustify />
-            </button>
-            <button className={styles.searchButton2}>
-              <FaSearch></FaSearch> Search
-            </button>
-          </div>
         </div>
       </div>
     </>
